e9ea19e7c2367893a28e8eb17edb7665587f1b83
Chris H <cholladay0816@gmail.com>
2021-01-19 23:48:36 -0600

    Update 2020_12_28_215139_create_commissions_table.php

--- database/migrations/2020_12_28_215139_create_commissions_table.php
+++ database/migrations/2020_12_28_215139_create_commissions_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\CommissionPreset;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +17,31 @@ class CreateCommissionsTable extends Migration
     {
         Schema::create('commissions', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class, 'buyer_id');
+            $table->foreignIdFor(User::class, 'creator_id');
+            $table->foreignIdFor(CommissionPreset::class, 'preset_id')->nullable();
+            $table->string('title');
+            $table->string('description');
+            $table->string('memo');
+            $table->decimal('price')->unsigned();
+            $table->integer('days_to_complete')->unsigned();
+            $table->dateTime('date_due')->nullable();
+
+            $table->enum('status',
+            [
+                'Unpaid',           // Created request
+                'Pending',          // Entered payment details, now visible and sent to creator
+                'Declined',         // Creator declined
+                'Capture-Failed',   // Failed to capture payment after being accepted
+                'Active',           // Accepted and payment success, order now started
+                'Overdue',          // Overdue order, may be canceled by client
+                'Canceled',         // Order canceled by creator
+                'Completed',        // Completed order, under review by client
+                'Disputed',         // Client disputed the finished product
+                'Refunded',         // Order was refunded
+                'Archived'          // Order was archived after completopm
+            ]);
+
             $table->timestamps();
         });
     }
