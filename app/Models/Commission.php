4f73577975874dff28dfdc3fd085057118b56962
Chris H <cholladay0816@gmail.com>
2021-01-19 23:48:15 -0600

    Update Commission.php

--- app/Models/Commission.php
+++ app/Models/Commission.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Commission extends Model
 {
     use HasFactory;
+    public function buyer()
+    {
+        return $this->belongsTo(User::class, 'buyer_id');
+    }
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
 }
