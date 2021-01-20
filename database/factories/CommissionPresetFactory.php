b09cdaf2eb9d4d89ac210ce78a9922d6ccac0998
Chris H <cholladay0816@gmail.com>
2021-01-19 23:57:24 -0600

    Create CommissionPresetFactory.php

---
+++ database/factories/CommissionPresetFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\CommissionPreset;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommissionPresetFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = CommissionPreset::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
