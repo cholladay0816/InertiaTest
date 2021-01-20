1877d814274325d73c54941149c0974e90de13b3
Chris H <cholladay0816@gmail.com>
2021-01-19 23:50:42 -0600

    Update CommissionController.php

--- app/Http/Controllers/CommissionController.php
+++ app/Http/Controllers/CommissionController.php
@@ -8,9 +8,38 @@ use Inertia\Inertia;
 
 class CommissionController extends Controller
 {
+    public function getCommissions()
+    {
+        $commissions = Commission::with('buyer', 'creator')->where('creator_id', auth()->user()->id)->get()->sortByDesc('created_at');
+        $overdue = $commissions->where('status', 'Overdue');
+        $active = $commissions->where('status', 'Active');
+        $pending = $commissions->where('status', 'Pending');
+        $disputed = $commissions->where('status', 'Disputed');
+        $completed = $commissions->where('status', 'Completed');
+        $history = $commissions->where('status', 'History');
+        $history->join($commissions->where('status', 'Canceled'));
+        $history->join($commissions->where('status', 'Refunded'));
+
+        return [
+            'commissions' => $commissions,
+            'overdue' => $overdue,
+            'active' => $active,
+            'pending' => $pending,
+            'disputed' => $disputed,
+            'completed' => $completed,
+            'finished' => $history
+        ];
+    }
+
     public function list()
     {
-        $commissions = Commission::all();
-        return Inertia::render('Commissions', ['commissions'=>$commissions]);
+        return Inertia::render('Commission/List', $this->getCommissions());
+    }
+
+    public function show(Commission $commission)
+    {
+        $props = $this->getCommissions();
+        $props['commission'] = $commission;
+        return Inertia::render('Commission/List', $props);
     }
 }
