067bb15f589b65de5dce5dd700bee74149cd0bc8
Chris H <cholladay0816@gmail.com>
2021-01-19 23:52:58 -0600

    Delete heading.blade.php

--- resources/views/components/welcome/heading.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="w-full h-16 bg-red-400">
-    <!-- Order your soul. Reduce your wants. - Augustine -->
-    <button class="text-blue-400" @click.native="toggle">TEST</button>
-        @if($popout)
-            <p :show="popout">Yes</p>
-        @endif
-</div>
